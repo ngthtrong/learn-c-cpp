af91209dc16e4bb34c0a8aba03892b30ca7ac6c2
ngthtrong204 <ngthtrong204@gmail.com>
2024-06-24 22:26:12 +0700

    Refactor linked list implementation and add function to filter and print passed students

--- CTDL/my-dataStructure-ex/2.LinkedList/main.c
+++ CTDL/my-dataStructure-ex/2.LinkedList/main.c
@@ -1,11 +1,46 @@
-typedef struct SinhVien
+#include <stdio.h>
+#include <stdlib.h> // Add this line to include the necessary header file
+
+typedef struct
 {
-   char mssv[10];
-   char name[50];
-   float lt, tt1, tt2;
-} SinhVien;
-typedef struct List
+   char ID[10];
+   char Name[50];
+   float R1, R2, R3;
+} Student;
+struct Node
 {
-   SinhVien A[40];
-   int n;
-} List;
+   Student Element;
+   struct Node *Next;
+};
+typedef struct Node* List;
+
+List getList(){
+   List L=(List)malloc(sizeof(struct Node));
+   L->Next=NULL;
+   return L;
+}
+
+void showPassedList(List L)
+{
+   List passList = getList();
+   passList->Next = NULL;
+   while (L->Next != NULL)
+   {
+      if((L->Next->Element.R1+L->Next->Element.R2+L->Next->Element.R3)/3>=4)
+      append(L->Next->Element,passList);     
+      L = L->Next;
+   }
+   printList(passList);
+}
+int append(Student s, List *pl){
+   
+}
+
+int main()
+{
+   List L = getList();
+   if (L->Next == NULL)
+      printf("Created a list!!!");
+   else
+      printf("Exception!!!");
+}
