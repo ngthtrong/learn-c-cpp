735ffd3f81bbd591c33c214f8dbabce778c76d6e
ngthtrong204 <ngthtrong204@gmail.com>
2024-06-26 10:13:57 +0700

    move to dathuc

--- CTDL/my-dataStructure-ex/2.LinkedList/DaThuc.c
+++ CTDL/my-dataStructure-ex/2.LinkedList/DaThuc.c
@@ -1,3 +1,50 @@
-int main(){
+#include <stdio.h>
+
+typedef struct
+{
+  double he_so;
+  int bac;
+} DonThuc;
+struct Node
+{
+  DonThuc e;
+  struct Node *Next;
+};
+typedef struct Node *Position;
+typedef Position DaThuc;
+//======================================================//
+DaThuc khoitao()
+{
+  DaThuc temp = (struct Node *)malloc(sizeof(struct Node));
+  temp->Next = NULL;
+  return temp;
+}
+void inDaThuc(DaThuc d)
+{
+  while (d->Next != NULL)
+  {
+    if (d->Next->Next == NULL)
+      printf("%.3fX^%d", d->Next->e.he_so, d->Next->e.bac);
+    else
+      printf("%.3fX^%d + ", d->Next->e.he_so, d->Next->e.bac);
+    d = d->Next;
+  }
+  printf("\n");
+}
+DaThuc nhanDaThuc(DaThuc d1, DaThuc d2)
+{
+  DaThuc temp = (struct Node *)malloc(sizeof(struct Node));
+  temp->Next=NULL;
+  
+
+
+
+
+  return temp;
+}
+
+//======================================================//
+int main()
+{
   printf("hello");
 }
