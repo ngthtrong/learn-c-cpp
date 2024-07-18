463aec76f56df7e18a643419697bc0d128d9845a
ngthtrong204 <ngthtrong204@gmail.com>
2024-07-18 13:18:03 +0700

    start BST, delete trietfile

--- CTDL/6-BST/basicFunc.c
+++ CTDL/6-BST/basicFunc.c
@@ -3,18 +3,21 @@
 
 struct Node
 {
-  int Key;
-  struct Node *Left, *Right;
+  int Key, key;
+  struct Node *Left, *Right, *left, *right;
 };
 typedef struct Node *Tree;
-
-void insertNode(int x, Tree *Root) {}
 
 Tree initTree()
 {
   Tree temp;
   temp = NULL;
   return temp;
+}
+
+int isEmpty(Tree tr)
+{
+  return tr == NULL;
 }
 
 void preOrder(Tree tr)
@@ -25,6 +28,42 @@ void preOrder(Tree tr)
     preOrder(tr->Left);
     preOrder(tr->Right);
   }
+}
+
+void in_order(Tree tr)
+{
+  if (tr != NULL)
+  {
+    in_order(tr->left);
+    printf("%d ", tr->key);
+    in_order(tr->right);
+  }
+}
+
+void post_order(Tree tr)
+{
+  if (tr != NULL)
+  {
+    post_order(tr->left);
+    post_order(tr->right);
+    printf("%d ", tr->key);
+  }
+}
+
+Tree search(int x, Tree tr)
+{
+  if (tr != NULL)
+  {
+    if (x > tr->key)
+      return search(x, tr->right);
+    else if (x < tr->key)
+      return search(x, tr->left);
+    else if (x == tr->key)
+    {
+      return tr;
+    }
+  }
+  return NULL;
 }
 
 Tree getNext(int x, Tree tr)
@@ -99,27 +138,27 @@ Tree getParent(int x, Tree tr)
   return NULL;
 }
 
-void printPath(int x, Tree tr)
+void print_path(int x, Tree tr)
 {
   {
     if (tr != NULL)
     {
-      printf("%d ", tr->Key);
-      if (x > tr->Key)
+      printf("%d ", tr->key);
+      if (x > tr->key)
       {
-        printPath(x, tr->Right);
+        print_path(x, tr->right);
       }
-      else if (x < tr->Key)
+      else if (x < tr->key)
       {
-        printPath(x, tr->Left);
+        print_path(x, tr->left);
       }
-      else if (x == tr->Key)
+      else if (x == tr->key)
       {
-        printf("-> Tim thay");
+        printf(" --> Tim thay");
       }
     }
     else
-      printf("-> Khong thay");
+      printf(" --> Tim khong thay");
   }
 }
 
@@ -142,6 +181,27 @@ void insertNode(int x, Tree *tr)
   }
 }
 
+Tree insert_node(int x, Tree root)
+{
+  if (root == NULL)
+  {
+    Tree temp = (Tree)malloc(sizeof(struct Node));
+    root = temp;
+    root->key = x;
+    root->right = NULL;
+    root->left = NULL;
+  }
+  else if (x < root->key)
+  {
+    root->left = insert_node(x, root->left);
+  }
+  else if (x > root->key)
+  {
+    root->right = insert_node(x, root->right);
+  }
+  return root;
+}
+
 void posOrder(Tree tr)
 {
   if (tr != NULL)
@@ -152,26 +212,131 @@ void posOrder(Tree tr)
   }
 }
 
-int getHeight(Tree tr)
+int get_height(Tree tr)
 {
   if (tr != NULL)
   {
-    if (tr->Left != NULL && tr->Right != NULL)
+
+    if (tr->left != NULL && tr->right != NULL)
     {
-      int tempR = getHeight(tr->Right);
-      int tempL = getHeight(tr->Left);
+      int tempR = get_height(tr->right);
+      int tempL = get_height(tr->left);
       return 1 + (tempR > tempL ? tempR : tempL);
     }
-    else if (tr->Left != NULL)
-      return 1 + getHeight(tr->Left);
+    else if (tr->left != NULL)
+      return 1 + get_height(tr->left);
     else
-      return 1 + getHeight(tr->Right);
+      return 1 + get_height(tr->right);
   }
   return -1;
 }
 
+int hNode(int x, Tree tr)
+{
+  if (tr != NULL)
+  {
+    if (x > tr->Key)
+      return hNode(x, tr->Right);
+    else if (x < tr->Key)
+      return hNode(x, tr->Left);
+    else if (x == tr->Key)
+    {
+      if (tr->Left != NULL && tr->Right != NULL)
+      {
+        int tempR = hNode(x,tr->Right);
+        int tempL = hNode(x,tr->Left);
+        return 1 + (tempR > tempL ? tempR : tempL);
+      }
+      else if (tr->Left != NULL)
+        return 1 + hNode(x,tr->Left);
+      else
+        return 1 + hNode(x,tr->Right);
+    }
+    return-1;
+  }
+  return 0;
+}
+
 void deleteNode(int x, Tree *tr)
 {
+  if ((*tr) != NULL)
+  {
+    if (x < (*tr)->Key)
+      deleteNode(x, &((*tr)->Left));
+    else if (x > (*tr)->Key)
+      deleteNode(x, &((*tr)->Right));
+    else
+    {
+      if ((*tr)->Left == NULL && (*tr)->Right == NULL)
+      {
+        free((*tr));
+        (*tr) = NULL;
+      }
+      else if ((*tr)->Left == NULL)
+      {
+        Tree delNode = (*tr);
+        (*tr) = (*tr)->Right;
+        free(delNode);
+        delNode = NULL;
+      }
+      else if ((*tr)->Right == NULL)
+      {
+        Tree delNode = (*tr);
+        (*tr) = (*tr)->Left;
+        free(delNode);
+        delNode = NULL;
+      }
+      else
+      {
+        Tree minNode = (*tr)->Right;
+        while (minNode->Left != NULL)
+          minNode = minNode->Left;
+        (*tr)->Key = minNode->Key;
+        deleteNode(minNode->Key, &((*tr)->Right));
+      }
+    }
+  }
+}
+
+Tree delete_node(int x, Tree *tr)
+{
+  if ((*tr) != NULL)
+  {
+    if (x < (*tr)->key)
+      delete_node(x, &((*tr)->left));
+    else if (x > (*tr)->key)
+      delete_node(x, &((*tr)->right));
+    else
+    {
+      if ((*tr)->left == NULL && (*tr)->right == NULL)
+      {
+        free((*tr));
+        (*tr) = NULL;
+      }
+      else if ((*tr)->left == NULL)
+      {
+        Tree delNode = (*tr);
+        (*tr) = (*tr)->right;
+        free(delNode);
+        delNode = NULL;
+      }
+      else if ((*tr)->right == NULL)
+      {
+        Tree delNode = (*tr);
+        (*tr) = (*tr)->left;
+        free(delNode);
+        delNode = NULL;
+      }
+      else
+      {
+        Tree minNode = (*tr)->right;
+        while (minNode->left != NULL)
+          minNode = minNode->left;
+        (*tr)->key = minNode->key;
+        delete_node(minNode->key, &((*tr)->right));
+      }
+    }
+  }
 }
 
 int main()
