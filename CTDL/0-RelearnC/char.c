31515482b6907ba6f7db548c92784720679bdb14
ngthtrong204 <ngthtrong204@gmail.com>
2024-08-07 12:45:20 +0700

    done first homework

---
+++ CTDL/0-RelearnC/char.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Student
+{
+  char Code[10];
+  char Name[50];
+} Student;
+
+void readClass(Student *m01, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    fgets(m01[i].Code, 10, stdin);
+    if (m01[i].Code[strlen(m01[i].Code) - 1] == '\n')
+      m01[i].Code[strlen(m01[i].Code) - 1] = '\0';
+    fgets(m01[i].Name, 50, stdin);
+    if (m01[i].Name[strlen(m01[i].Name) - 1] == '\n')
+      m01[i].Name[strlen(m01[i].Name) - 1] = '\0';
+  }
+}
+
+void printClass(Student *m01, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    printf("%d: %s - %s\n", i + 1, m01[i].Code, m01[i].Name);
+  }
+}
+
+void swap(Student *m01, int a, int b)
+{
+  Student temp = m01[a];
+  m01[a] = m01[b];
+  m01[b] = temp;
+}
+
+void sort(Student *m01, int n)
+{
+  for (int i = 0; i < n - 1; i++)
+  {
+    for (int j = n - 1; j > i; j--)
+    {
+      if (strcmp(m01[j - 1].Code, m01[j].Code) > 0)
+        swap(m01, j - 1, j);
+    }
+  }
+  printClass(m01, n);
+}
+
+int main()
+{
+  int n;
+  printf("Nhap si so lop: ");
+  scanf("%d\n", &n);
+  Student m01[n];
+  readClass(m01, n);
+  sort(m01, n);
+}
---
+++ CTDL/0-RelearnC/int.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Student
+{
+  int Code;
+  char Name[50];
+} Student;
+
+void readClass(Student *m01, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    scanf("%d\n", &m01[i].Code);
+    fgets(m01[i].Name, 50, stdin);
+    if (m01[i].Name[strlen(m01[i].Name) - 1] == '\n')
+      m01[i].Name[strlen(m01[i].Name) - 1] = '\0';
+  }
+}
+
+void printClass(Student *m01, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    printf("%d: %5d - %s\n", i + 1, m01[i].Code, m01[i].Name);
+  }
+}
+
+void swap(Student *m01, int a, int b)
+{
+  Student temp = m01[a];
+  m01[a] = m01[b];
+  m01[b] = temp;
+}
+
+void sort(Student *m01, int n)
+{
+  for (int i = 0; i < n - 1; i++)
+  {
+    for (int j = n - 1; j > i; j--)
+    {
+      if (m01[j-1].Code > m01[j].Code)
+        swap(m01, j - 1, j);
+    }
+  }
+  printClass(m01, n);
+}
+
+int main()
+{
+  int n;
+  printf("Nhap si so lop: ");
+  scanf("%d\n", &n);
+  Student m01[n];
+  readClass(m01, n);
+  sort(m01, n);
+}
