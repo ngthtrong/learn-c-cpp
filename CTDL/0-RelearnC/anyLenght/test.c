b28f3f1733576f0856456cb9eedf85ede570ec2f
ngthtrong <ngthtrong204@gmail.com>
2024-08-28 08:36:50 +0700

    remove all file

--- CTDL/0-RelearnC/anyLenght/test.c
+++ CTDL/0-RelearnC/anyLenght/test.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
-int main(){
-    int a[4] = {1,2,3,4};
-    printf("%d",a[10]);
+typedef int ElementType;
+typedef int Position;
+#define MaxLenght 100
+typedef struct List
+{
+    ElementType data[MaxLenght];
+    int size;
+} List;
+
+Position locate(ElementType x, List l)
+{
+    for (Position i = 0; i < l.size; i++)
+    {
+        if (l.data[i] == x)
+        {
+            return i;
+        }
+    }
+    return (Position)l.size;
+}
+
+int main()
+{
+    List l = {{1, 2, 3, 4, 5}, 5};
+    printf("%d", locate(456, l));
 }
