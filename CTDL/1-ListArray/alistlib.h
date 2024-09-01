ffe8fe6843f681450af7b49937f6669405758283
ngthtrong <ngthtrong204@gmail.com>
2024-09-01 20:19:21 +0700

    done alistlib.h

--- CTDL/1-ListArray/alistlib.h
+++ CTDL/1-ListArray/alistlib.h
@@ -4,175 +4,244 @@
 
 typedef int Position;
 typedef int ElementType;
-typedef struct{
-  ElementType elements[MAXSIZE];
-  Position size;
-}List;
+typedef struct
+{
+	ElementType elements[MAXSIZE];
+	Position size;
+} List;
 List L;
 /*
 Initialize an empty list
 */
-void makeNull(List *L){
-    L->elements[0] = '\0';
-    L->size =0;
-
+void makeNull(List *L)
+{
+	L->elements[0] = '\0';
+	L->size = 0;
 }
 
 /*
 return Number of elements
 */
-int len(List L){
+int len(List L)
+{
 	return L.size;
 }
 
 /*
 Check whether the list is empty?
 */
-int empty(List L){
-	return (L.size==0);
+int empty(List L)
+{
+	return (L.size == 0);
 }
 
 /*
 Check whether the list is full?
 */
-int fullList(List L){
-	return (L.size==MAXSIZE);
-	
+int fullList(List L)
+{
+	return (L.size == MAXSIZE);
 }
 
 /*
 Traverse the list to print out all elements
 */
-void print(List L){
+void print(List L)
+{
 	Position i;
-	for(i=1; i<=L.size;++i) printf("%d ", L.elements[i-1]);
+	for (i = 1; i <= L.size; ++i)
+		printf("%d ", L.elements[i - 1]);
 	printf("\n");
 }
 
 /*
 Return the element at position p
 */
-ElementType getAt(Position p, List L){
-
-
-
+ElementType getAt(Position p, List L)
+{
+	if (p >= 1 && p <= L.size)
+		return L.elements[p - 1];
+	printf("Error Position not found!\n");
+	exit(EXIT_FAILURE);
+}
+ElementType retrieve(Position p, List L)
+{
+	if (p < L.size)
+		return L.elements[p - 1];
+	printf("Error Position not found!\n");
+	exit(EXIT_FAILURE);
 }
 
 /*
 Return the first position in L
 */
-Position first(List L){
-
-
-
+Position first(List L)
+{
+	return 1;
 }
 
 /*
 Return the end position in L
 */
-Position endList(List L){
-
-
-
+Position endList(List L)
+{
+	if (empty(L))
+		return 1;
+	else
+		return L.size + 1;
 }
 
 /*
 Update the element at position p by a new value
 */
-void setAt(Position p, ElementType x,List *L){
-
-
-
+void setAt(Position p, ElementType x, List *L)
+{
+	if (p >= 1 && p <= L->size)
+		L->elements[p - 1] = x;
+	else
+	{
+		printf("Error Position not found!\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
 /*
 Insert x at position p
 */
-void insertAt(Position p, ElementType x, List *L){
-
-
-
+void insertAt(Position p, ElementType x, List *L)
+{
+	if (p >= 1 && p <= L->size + 1)
+	{
+		for (Position i = L->size; i >= p; i--)
+			L->elements[i] = L->elements[i - 1];
+		L->elements[p - 1] = x;
+		L->size++;
+	}
+	else
+	{
+		printf("Error Position not found!\n");
+		exit(EXIT_FAILURE);
+	}
 }
 /*
 Remove and return the element at position p
 */
-ElementType popAt(Position p, List *L){
-	ElementType x;
-	if(p>=1 && p<=L->size){
-		x= L->elements[p-1];
-		Position q;	
-		for(q=p;q<endList(*L);q++)
-			L->elements[q-1]=L->elements[q];	
+ElementType popAt(Position p, List *L) // delete
+{
+	if (p >= 1 && p <= L->size)
+	{
+		ElementType x = L->elements[p - 1];
+		for (Position i = p; i < endList(*L); i++)
+			L->elements[i - 1] = L->elements[i];
 		L->size--;
 		return x;
 	}
-	else{ 
+	else
+	{
 		printf("Position Error! Exit....\n");
 		exit(EXIT_FAILURE);
-	}	
-	
-	
+	}
 }
 
 /*
 Insert x to the first position
 */
-void insertFirst(ElementType x, List *L){
-
-
+void insertFirst(ElementType x, List *L)
+{
+	if (empty(*L))
+	{
+		L->elements[0] = x;
+		L->size++;
+	}
+	else
+		insertAt(first(*L), x, L);
 }
 
-/*
-Remove and return the first element
-*/
-ElementType popFirst(List *L){
-	return popAt(first(*L), L);
-	
-}
 /*
 Append a new element to the list
 */
-void append(ElementType x,List *L){
-
-
-
+void append(ElementType x, List *L)
+{
+	insertAt(endList(*L), x, L);
+}
+/*
+Remove and return the first element
+*/
+ElementType popFirst(List *L)
+{
+	return popAt(first(*L), L);
 }
 
 /*
 Remove and return the last element
 */
-ElementType popLast(List *L){
-	return popAt(endList(*L), L);
+ElementType popLast(List *L)
+{
+	return popAt(endList(*L) - 1, L);
 }
 
 /*
 Return the position of the first appearance of x in the list
 */
-Position locate(ElementType x, List L){
+Position locate(ElementType x, List L)
+{
 	Position p;
-	int found=0;
-	for(p=first(L);p<endList(L) && !found;p++)
-			if (L.elements[p-1]==x) found=1;
-		return p;
+	int found = 0;
+	for (p = first(L); p < endList(L) && !found; p++)
+		if (L.elements[p - 1] == x)
+			found = 1;
+	return p;
 }
 
 /*
 Return the next position of p in L
 */
-Position next(Position p,List L){
+Position next(Position p, List L)
+{
+	if (p >= 1 && p <= L.size && p != L.size)
 
-	
+		return p + 1;
+	else
+	{
+		printf("p is end of list\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
 /*
 Return the previous position of p in L
 */
-Position previous(Position p,List L){
-	if(p>=1 && p<=L.size){
-		return p-1;
+Position previous(Position p, List L)
+{
+	if (p >= 1 && p <= L.size && p != 1)
+	{
+		return p - 1;
 	}
-	else{ 
+	else
+	{
 		printf("Position Error! Exit....\n");
 		exit(EXIT_FAILURE);
-	}	
+	}
+}
+// void sort(List *L)
+// {
+// 	for (Position i = 1; !; i++)
+// 	{
+// 		/* code */
+// 	}
+// }
+void delete_duplicate(List *L)
+{
+	for (Position i = 1; i != endList(*L) && i != endList(*L); i++)
+	{
+		ElementType nowPosition = getAt(i, *L);
+		for (Position j = i + 1; j != endList(*L); j++)
+		{
+			ElementType temp = getAt(j, *L);
+			if (nowPosition == temp)
+			{
+				popAt(j, L);
+			}
+		}
+	}
 }
