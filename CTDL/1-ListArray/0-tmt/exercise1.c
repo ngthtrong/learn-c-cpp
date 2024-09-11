7a353bf431807bc5dbe0257ea7917fb80ea42d09
ngthtrong <ngthtrong204@gmail.com>
2024-09-11 23:06:26 +0700

    done exer1

--- CTDL/1-ListArray/0-tmt/exercise1.c
+++ CTDL/1-ListArray/0-tmt/exercise1.c
@@ -5,11 +5,17 @@ int main(int argc, char const *argv[])
 {
     List L;
     makeNull(&L);
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 15; i++)
     {
         ElementType rd_num = rand() % 100;
         insertFirst(rd_num, &L);
     }
+    insertAt(16, 0, &L);
+    insertAt(17, 10, &L);
+    insertAt(18, 20, &L);
+    insertAt(19, 30, &L);
+    insertAt(20, 40, &L);
+    insertAt(16, 100, &L);
     printf("List after input 20 ramdom integer:\n");
     print(L);
     sort(&L);
--- CTDL/1-ListArray/0-tmt/polynomials.c
+++ CTDL/1-ListArray/0-tmt/polynomials.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_SIZE 10
+#define MAX_SIZE 100
 
 typedef struct Monomial
 {
@@ -14,49 +14,118 @@ typedef struct Pylonomial
     int size;
 } Pylonomial;
 
-void simplification(Pylonomial *p) {};
+void sort(Pylonomial *L)
+{
+    for (int i = 0; i < L->size - 1; i++)
+    {
+        for (int j = L->size - 1; j > i; j--)
+        {
+            if (L->data[j - 1].exponent < L->data[j].exponent)
+            {
+                Monomial temp = L->data[j];
+                L->data[j] = L->data[j - 1];
+                L->data[j - 1] = temp;
+            }
+        }
+    }
+}
+
+void simplification(Pylonomial *p)
+{
+    sort(p);
+    for (int i = 0; i < p->size; i++)
+    {
+        if (p->data[i].exponent == p->data[i + 1].exponent)
+        {
+            p->data[i].factor += p->data[i + 1].factor;
+            for (int j = i + 1; j < p->size; j++)
+                p->data[j] = p->data[j + 1];
+            p->size--;
+            i--;
+        }
+        else if (p->data[i].factor == 0)
+        {
+            for (int j = i; j < p->size; j++)
+                p->data[j] = p->data[j + 1];
+            p->size--;
+            i--;
+        }
+    }
+};
 void derivative(Pylonomial *p)
 {
+    simplification(p);
+
     for (int i = 0; i < p->size; i++)
     {
         if (p->data[i].exponent == 0)
         {
             p->size--;
-            p->data[i] = '\0';
         }
         else
         {
-            p->data[i].factor *= p->data[i].exponent--;
+            float tempExponent = (float)p->data[i].exponent--;
+            p->data[i].factor *= tempExponent;
         }
     }
 };
-Pylonomial add(Pylonomial a, Pylonomial b)
+Pylonomial add(Pylonomial *a, Pylonomial *b)
 {
-    for (int i = a.size; i < a.size + b.size; i++)
-        a.data[i] = b.data[i - a.size];
-    simplification(&a);
-    return a;
+    Pylonomial temp;
+    temp.size = a->size + b->size;
+    for (int i = 0; i < a->size; i++)
+        temp.data[i] = a->data[i];
+    for (int i = 0; i < b->size; i++)
+        temp.data[i + a->size] = b->data[i];
+    simplification(&temp);
+    return temp;
 };
 
 void print(Pylonomial p)
 {
     for (int i = 0; i < p.size; i++)
-        printf("%.3fx^%d ", p.data[i].factor, p.data[i].exponent);
+    {
+        if (p.data[i].exponent == 0)
+            printf("%.0f ", p.data[i].factor);
+        else if (p.data[i].exponent == 1)
+            printf("%.0fx ", p.data[i].factor);
+        else
+            printf("%.0fx^%d ", p.data[i].factor, p.data[i].exponent);
+        if (i != p.size - 1)
+            printf("+ ");
+    }
     printf("\n");
 };
 void read(Pylonomial *p)
 {
     int n;
+    printf("Enter number monomial of pylonomial: ");
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
+        printf("Enter monomial %d: ", i);
         Monomial temp;
-        scanf("%f %d", temp.factor, temp.exponent);
+        scanf("%f %d", &temp.factor, &temp.exponent);
         p->data[i] = temp;
     }
+    p->size = n;
 };
 int main(int argc, char const *argv[])
 {
-
+    Pylonomial a, b;
+    a.size = 0;
+    b.size = 0;
+    read(&a);
+    read(&b);
+    printf("a %d: ", a.size);
+    print(a);
+    printf("b %d: ", b.size);
+    print(b);
+    Pylonomial sum = add(&a, &b);
+    printf("sum: ");
+    print(sum);
+    derivative(&a);
+    printf("derivative of a: ");
+    print(a);
     return 0;
 }
