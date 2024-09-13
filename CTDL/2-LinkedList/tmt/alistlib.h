63181dc39e04c0065eaaba6ddd2138bf16ee88f7
ngthtrong <ngthtrong204@gmail.com>
2024-09-13 16:39:23 +0700

    done exer1

--- CTDL/2-LinkedList/tmt/alistlib.h
+++ CTDL/2-LinkedList/tmt/alistlib.h
@@ -1,33 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef int ElementType;
-typedef struct NodeTag
+typedef struct  Node
 {
     ElementType data;
-    struct Nodetag *next;
-} Node;
+    struct Node *next;
+}Node ;
+// typedef struct Node Node;
 typedef Node *List;
 typedef Node *Position;
 
 void makeNull(List *L)
 {
     *L = (Node *)malloc(sizeof(Node));
-    (*L)->next = NULL;
+    (*L)->next->data = NULL;
 };
-int empty(List L) {
-    return L->next==NULL;
+int empty(List L)
+{
+    return L->next == NULL;
 };
 int len(List L)
 {
+    Node *p = L;
     int count = 0;
-    while (!empty(L))
+    while (p->next != NULL)
+    {
         count++;
+        p = p->next;
+    }
     return count;
 };
-int fullList(List L) {};
-void print(List L) {};
-ElementType getAt(Position p, List L) {};
+int fullList(List L)
+{
+    return 1;
+};
+void print(List L)
+{
+    Node *p = L;
+    while (p->next != NULL)
+    {
+        printf("%d ", p->next->data);
+        p = p->next;
+    }
+};
+ElementType getAt(Position p, List L)
+{
+    if (p->next != NULL)
+        return p->next->data;
+    exit(EXIT_FAILURE);
+};
 Position first(List L) {};
 Position endList(List L) {};
 void setAt(Position p, ElementType x, List *L) {};
---
+++ CTDL/2-LinkedList/tmt/linkedlib.c
@@ -0,0 +1,198 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int ElementType;
+
+typedef struct Node
+{
+    ElementType data;
+    struct Node *next;
+} Node;
+typedef Node *Position;
+typedef Position List;
+
+void makeNull(List *L)
+{
+    *L = (Node *)malloc(sizeof(Node));
+    (*L)->next = NULL;
+};
+
+int empty(List L)
+{
+    return L->next == NULL;
+};
+int len(List L)
+{
+    Node *p = L;
+    int count = 0;
+    while (p->next != NULL)
+    {
+        count++;
+        p = p->next;
+    }
+    return count;
+};
+int fullList(List L)
+{
+    return 1;
+};
+void print(List L)
+{
+    Node *p = L;
+    while (p->next != NULL)
+    {
+        printf("%d ", p->next->data);
+        p = p->next;
+    }
+    printf("\n");
+};
+ElementType getAt(Position p, List L)
+{
+    if (p->next != NULL)
+        return p->next->data;
+    exit(EXIT_FAILURE);
+};
+
+Position first(List L)
+{
+    if (!empty(L))
+        return L;
+    printf("err first: empty list\n");
+    exit(EXIT_FAILURE);
+};
+Position endList(List L)
+{
+    Position p = L;
+    while (p->next != NULL)
+        p = p->next;
+    return p;
+};
+void setAt(Position p, ElementType x, List *L)
+{
+    if (p->next != NULL)
+        p->next->data = x;
+    exit(EXIT_FAILURE);
+};
+void myInsertAt(Position p, ElementType x, List *L)
+{
+    Node *temp = (Node *)malloc(sizeof(Node));
+    temp->data = x;
+    temp->next = p->next;
+    p->next = temp;
+};
+
+void insertAt(int pos, ElementType x, List *L)
+{
+    Position p = (*L);
+    for (int i = 1; i < pos; i++)
+        p = p->next;
+
+    Node *temp = (Node *)malloc(sizeof(Node));
+    temp->data = x;
+    temp->next = p->next;
+    p->next = temp;
+};
+ElementType popAt(Position p, List *L)
+{
+    if (p->next != NULL)
+    {
+        ElementType x = p->next->data;
+        Position temp = p->next;
+        p->next = p->next->next;
+        free(temp);
+        return x;
+    }
+    printf("err popAt: end of list\n");
+    exit(EXIT_FAILURE);
+};
+void insertFirst(ElementType x, List *L)
+{
+    Node *temp = (Node *)malloc(sizeof(Node));
+    temp->data = x;
+    temp->next = (*L)->next;
+    (*L)->next = temp;
+};
+ElementType popFirst(List *L)
+{
+    if (!empty(*L))
+    {
+        Position fs = (*L);
+        ElementType x = fs->next->data;
+        (*L)->next = (*L)->next->next;
+        free(fs->next);
+        return x;
+    }
+    printf("err popFirst: empty list\n");
+    exit(EXIT_FAILURE);
+};
+void append(ElementType x, List *L)
+{
+    Node *temp = (Node *)malloc(sizeof(Node));
+    temp->data = x;
+    temp->next = NULL;
+    Position end = endList(*L);
+    end->next = temp;
+};
+ElementType popLast(List *L)
+{
+    Position end = endList(*L);
+    ElementType x= end->data;
+    Position p = *L;
+    while (p->next != end)
+        p = p->next;
+    p->next = NULL;
+    free(end);
+    return x;
+};
+Position locate(ElementType x, List L)
+{
+    if (!empty(L))
+    {
+        Position p = L;
+        while (p->next->data != x)
+        {
+            if (p->next == NULL)
+            {
+                printf("err locate: not found x\n");
+                exit(EXIT_FAILURE);
+            }
+            p = p->next;
+        }
+        return p;
+    }
+    printf("err locate: empty list\n");
+    exit(EXIT_FAILURE);
+};
+Position next(Position p, List L)
+{
+    if (p->next == NULL)
+    {
+        printf("err next: end of list\n");
+        exit(EXIT_FAILURE);
+    }
+    return p->next;
+};
+Position previous(Position p, List L)
+{
+    if (p == first(L))
+    {
+        printf("err previous: first of list\n");
+        exit(EXIT_FAILURE);
+    }
+    Position temp = L;
+    while (temp->next != p)
+    {
+        if (temp->next == NULL)
+        {
+            printf("err previous: position doesn't exist\n");
+            exit(EXIT_FAILURE);
+        }
+        temp = temp->next;
+    }
+    return temp;
+};
+
+void bubbleSort(List *L){
+    printf("hello world");
+};
