9f96a997df7eb64e4b76ab211e7252f7c3c748d5
ngthtrong <ngthtrong204@gmail.com>
2024-09-25 09:37:50 +0700

    lkdsajf

--- CTDL/3-Stack/basicFunc.c
+++ CTDL/3-Stack/basicFunc.c
@@ -61,7 +61,61 @@ void to_binary(int n)
   }
   print(&s);
 }
+
+int calFibo(int n)
+{
+  Stack s;
+  makenull(&s);
+  push(n, &s);
+  int rs = 0;
+  while (!is_empty(s))
+  {
+    int temp = pop(&s);
+    if (temp > 2)
+    {
+      push(temp - 2, &s);
+      push(temp - 1, &s);
+    }
+    else{
+      rs+=1;
+    }
+  }
+  return rs;
+}
+int calC(int k, int n){
+  Stack ks;
+  makenull(&ks);
+  Stack ns;
+  makenull(&ns);
+  int rs = 0;
+  push(k,&ks);
+  push(n,&ns);
+  while(!(is_empty(ks)&&is_empty(ns))){
+      int tempK = pop(&ks);
+      int tempN = pop(&ns);
+      if(tempK == 0|| tempK==tempN)
+        rs+=1;
+      else if(tempK==1||tempK==tempN-1)
+        rs += tempN;
+      else {
+        push(tempK-1,&ks);
+        push(tempK,&ks);
+        push(tempN-1,&ns);
+        push(tempN-1,&ns);
+      }
+  }
+  return rs;
+
+
+}
+
+
+
 int main()
 {
-  to_binary(0);
+//  int rs =  calFibo(7);
+//  printf("%d\n",rs);
+ int c =  calC(3,5);
+ printf("%d\n",c);
+  return 0;
 }
