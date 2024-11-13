f05e916fba2e91dd01beb2cf770881c272956364
ngthtrong <ngthtrong204@gmail.com>
2024-11-13 09:52:23 +0700

    w15

--- CTDL/6-BST/tmt/main.c
+++ CTDL/6-BST/tmt/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef char DataType;
+typedef int DataType;
 struct Node
 {
     DataType key;
@@ -36,16 +36,6 @@ int isLeaf(Tree tr)
     return 0;
 }
 
-Tree create2(DataType x, Tree l, Tree r)
-{
-    Tree tr;
-    tr = (Tree)malloc(sizeof(struct Node));
-    tr->key = x;
-    tr->left = l;
-    tr->right = r;
-    return tr;
-}
-
 void preOrder(Tree tr)
 {
     if (tr != NULL)
@@ -61,7 +51,7 @@ void inOrder(Tree tr)
     if (tr != NULL)
     {
         inOrder(tr->left);
-        printf("%c ", tr->key);
+        printf("%d ", tr->key);
         inOrder(tr->right);
     }
 }
@@ -76,37 +66,155 @@ void postOrder(Tree tr)
     }
 }
 
-int countNode(Tree tr)//O(n)
+int countNode(Tree tr) // O(n)
 {
     if (tr != NULL)
         return countNode(tr->right) + countNode(tr->left) + 1;
     return 0;
 }
-int countLeaf(Tree tr)//O(n)
+int countLeaf(Tree tr) // O(n)
 {
     if (tr != NULL)
     {
         if (isLeaf(tr))
             return 1;
-        return  countLeaf(tr->right) + countLeaf(tr->left);
+        return countLeaf(tr->right) + countLeaf(tr->left);
     }
     return 0;
 }
+
+Tree search(DataType x, Tree tr)
+{
+    if (tr == NULL)
+        return NULL;
+
+    if (x == tr->key)
+        return tr;
+    else if (x < tr->key)
+        return search(x, tr->left);
+    else
+        return search(x, tr->right);
+}
+
+void insertNode(DataType x, Tree *tr)
+{
+    if ((*tr) != NULL)
+    {
+        if (x < (*tr)->key)
+            insertNode(x, &((*tr)->left));
+        else if (x > (*tr)->key)
+            insertNode(x, &((*tr)->right));
+    }
+    else
+    {
+        makeNull(tr);
+        (*tr)->key = x;
+    }
+}
+
+DataType deleteMin(Tree *tr)
+{
+
+    if ((*tr)->left != NULL)
+        return deleteMin(&((*tr)->left));
+    else
+    {
+        Tree temp = (*tr);
+        DataType x = temp->key;
+        (*tr)->left = temp->right;
+    
+        free(temp);
+        printf("%d\n", x);
+
+        return x;
+    }
+}
+
+
+
+DataType deleteMax(Tree *tr)
+{
+
+    if ((*tr)->right != NULL)
+        return deleteMin(&((*tr)->right));
+    else
+    {
+        Tree temp = (*tr);
+        DataType x = temp->key;
+        (*tr)->right = temp->left;
+    
+        free(temp);
+        printf("%d\n", x);
+
+        return x;
+    }
+}
+
+
+
+
+void deleteNode(int x, Tree *tr)
+{
+    if ((*tr) == NULL)
+        return;
+    if (x < (*tr)->key)
+        deleteNode(x, &((*tr)->left));
+    else if (x > (*tr)->key)
+        deleteNode(x, &((*tr)->right));
+    else
+    {
+        if ((*tr)->left == NULL && (*tr)->right == NULL)
+        {
+            Tree temp = (*tr);
+            (*tr) = NULL;
+            free(temp);
+        }
+        else if ((*tr)->left != NULL && (*tr)->right == NULL)
+        {
+            Tree temp = (*tr);
+            (*tr) = (*tr)->left;
+            free(temp);
+        }
+        else if ((*tr)->right != NULL && (*tr)->left == NULL)
+        {
+            Tree temp = (*tr);
+            (*tr) = (*tr)->right;
+            free(temp);
+        }
+        else
+        {
+            (*tr)->key = deleteMax(&((*tr)->right));
+        }
+    }
+}
+
+Tree readTree()
+{
+    Tree T = NULL;
+    int a[] = {13, 3, 4, 14, 1, 2, 18, 12, 10, 5, 11, 8, 7, 6, 9};
+    for (int i = 0; i < sizeof(a) / sizeof(int); i++)
+    {
+        insertNode(a[i], &T);
+    }
+    return T;
+}
+
 int main()
 {
-    Tree tr;
-    makeNull(&tr);
-    tr = create2('*', tr->left, tr->right);
-    tr->left = create2('+', create2('+', create2('+', create2('a', NULL, NULL), create2('b', NULL, NULL)), create2('*', create2('c', NULL, NULL), create2('+', create2('d', NULL, NULL), create2('e', NULL, NULL)))), create2('f', NULL, NULL));
-    tr->right = create2('+', create2('g', NULL, NULL), create2('f', NULL, NULL));
+    Tree tr = readTree();
+
     inOrder(tr);
     printf("\n");
-    preOrder(tr);
+    deleteNode(5, &tr);
+    deleteNode(2, &tr);
+    deleteNode(3, &tr);
+    deleteNode(4, &tr);
+
+    inOrder(tr);
     printf("\n");
-    printf("node: %d", countNode(tr));
-    printf("\n");
-    printf("leaf: %d", countLeaf(tr));
-    printf("\n");
+
+    // printf("%d\n", countLeaf(tr));
+    // printf("%d\n", countNode(tr));
 
     return 0;
 }
