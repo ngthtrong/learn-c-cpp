49e488cf75889cc5c45467662a025bbb3cb3aaa9
ngthtrong204 <ngthtrong204@gmail.com>
2025-04-21 14:48:45 +0700

    create CT175

---
+++ CT175-GraphTheory/hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+   vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+   int a =0;
+   a= 1;
+   cout<<a<<endl;
+   for (const string& word : msg)
+   {
+      cout << word << " ";
+   }
+   cout << endl;
+}
---
+++ CT175-GraphTheory/intro.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    printf("This is a test of the C programming language.\n");
+    
+    return 0;
+}
